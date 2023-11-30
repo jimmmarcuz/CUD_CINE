89676efe90f8926f3a45af5f6efd014c90687428
jimmmarcuz <mediunjimm@gamil.com>
2023-11-30 16:34:25 -0500

    primer commit

---
+++ CUD_CINE-master/ADMINISTRACION.WEB/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using ADMINISTRACION.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ADMINISTRACION.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LstCine()
+        {
+            return View();
+        }
+
+        public IActionResult AddEditCine()
+        {
+            return View();
+        }
+        public IActionResult LstPelicula()
+        {
+            return View();
+        }
+
+        public IActionResult AddEditPelicula()
+        {
+            return View();
+        }
+
+        public IActionResult LstPromocion()
+        {
+            return View();
+        }
+
+        public IActionResult AddEditPromocion()
+        {
+            return View();
+        }
+
+        public IActionResult LstEvento()
+        {
+            return View();
+        }
+
+        public IActionResult AddEditEvento()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CUD_CINE-master/ADMINISTRACION.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADMINISTRACION.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
